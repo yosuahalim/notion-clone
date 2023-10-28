c589ca04babda64f156eb2cc4f55af6765a66392
Yosua <yosua.halim@gmail.com>
2023-10-28 10:43:45 +0700

    arcihve function

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -41,7 +41,7 @@ const DocumentPage = () => {
         className="hidden dark:block"
       />
       <h2 className="text-lg font-medium">
-        Welcome to {user?.firstName}&apos;s Jotion
+        Welcome to {user?.firstName}&apos;s Yotion
       </h2>
       <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
--- src/app/(main)/_components/navigation.tsx
+++ src/app/(main)/_components/navigation.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
@@ -148,6 +149,7 @@ export const Navigation = () => {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
         </div>
         <div
           onMouseDown={handleMouseDown}
